29f2c734aa670888b55a79ef0af569466056a822
AntipinVlad <antipinmail1@gmail.com>
2024-03-21 19:31:13 +0200

    fix variable naming

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Dog dog1 = new Dog();
-        Dog dog2 = new Dog();
-        Cat cat1 = new Cat();
+        Dog cooper = new Dog();
+        Dog milo = new Dog();
+        Cat alvin = new Cat();
 
-        dog1.run(200);
-        dog1.swim(8);
-        dog2.run(800);
-        dog2.swim(30);
-        cat1.run(200);
-        cat1.swim(5);
+        cooper.run(200);
+        cooper.swim(8);
+        milo.run(800);
+        milo.swim(30);
+        alvin.run(200);
+        alvin.swim(5);
 
         System.out.println("Number of dogs: " + Dog.getCount());
         System.out.println("Number of cats: " + Cat.getCount());
